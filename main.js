c9e64877a5998617c52f3b7620d0aa5e9a78b606
UncomfortableChai <mj1055119@gmail.com>
2022-03-24 22:33:30 -0700

    adding funciotnality to page

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+//opacity .7 light green: #BBDBB4B3
+//light green: #BBDBB4;
+//opacity .7 dark green: #223021b3;
+//dark green: #223021;
+//dark purple: #302131;
+//light purple: #d3b3db;
+//  1.I need to make a refernce to the html body
+//  2.reference to a tags
+// 3.reference to the slider container
+// 4.refernce to the slider
 console.log("no qema cuh");
+const body = document.querySelector("body");
+const aTag = document.querySelectorAll("a");
+const sliderContainer = document.querySelector("#darkToggle");
+const slider = document.querySelector("#circle");
+console.log(aTag);
+
+slider.addEventListener("click", function () {
+    console.log("clicked")
+    this.classList.toggle("slider--left")
+});
